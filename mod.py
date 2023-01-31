c2d4060d83cd4438d678d98c82d3a3d34d961190
perezchris3599 <kweeziperez712@gmail.com>
2023-01-31 13:39:25 +0300

    sample modern gui part one

---
+++ mod.py
@@ -0,0 +1,30 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("500x350")
+
+def login():
+    print("Test")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System", text_font=("Roboto", 24))
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=Login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
