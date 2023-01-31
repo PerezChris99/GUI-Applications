a52478b09f73dc44cbe054fb8202ac00b704836c
perezchris3599 <kweeziperez712@gmail.com>
2023-01-31 14:59:43 +0300

    frame working

--- mod.py
+++ mod.py
@@ -1,7 +1,7 @@
 import customtkinter
 
-customtkinter.set_appearance_mode("system")
-customtkinter.set_default_color_theme("dark-blue")
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("green")
 
 root = customtkinter.CTk()
 root.geometry("500x350")
@@ -12,7 +12,7 @@ def login():
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(master=frame, text="Login System", text_font=("Roboto", 24))
+label = customtkinter.CTkLabel(master=frame, text="Login System", font=("Roboto", 24))
 label.pack(pady=12, padx=10)
 
 entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
@@ -21,7 +21,7 @@ entry1.pack(pady=12, padx=10)
 entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
 entry2.pack(pady=12, padx=10)
 
-button = customtkinter.CTkButton(master=frame, text="Login", command=Login)
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
 button.pack(pady=12, padx=10)
 
 checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
